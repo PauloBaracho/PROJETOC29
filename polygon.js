cd02a5666904016ce48fdc607fb09922c7b9f3b0
Paulo Hilton <pahbaracho@gmail.com>
2021-11-06 20:16:23 -0300

    Criei a Classe do Polygon

--- polygon.js
+++ polygon.js
@@ -1,28 +1,31 @@
 class Polygon{
-	constructor(x,y,r) {
-
-		var options ={
-			restitution:0.3,
-			friction:5,
-			density:1
+		constructor(x,y,r)
+		{
+			var options={
+				isStatic:false,
+				restitution:0,
+				friction:1,
+				density:1.2
+				}
+			this.x=x;
+			this.y=y;
+			this.r=r
+			this.image=loadImage("polygon.png");
+			this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+			World.add(world, this.body);
+	
 		}
-
-		this.x=x;
-		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-        this.imagem = loadImage("polygon.png")
-		World.add(world, this.body);
+		display()
+		{
+				var stonepos=this.body.position;		
+				push()
+				translate(stonepos.x, stonepos.y);
+				fill(255,0,255)
+				imageMode(CENTER);
+				ellipseMode(RADIUS)
+				image(this.image, 0,0,this.r*2, this.r*2)
+				pop()
+				
+		}
+	
 	}
-	display() {
-		var rubberpos=this.body.position;		
-		push()
-		translate(rubberpos.x, rubberpos.y);
-		rectMode(CENTER)
-		strokeWeight(4);
-		stroke("black");
-		fill("darkblue");
-		image (this.imagem, 0, 0, this.r, this.r);
-		pop()
-	}
-}
